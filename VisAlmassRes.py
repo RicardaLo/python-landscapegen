011cceb23836bd73096d784a660fb4bba3fc10b5
LDalby <lars@bios.au.dk>
2016-04-08 09:05:12 +0200

    Def spatial ref

--- VisAlmassRes.py
+++ VisAlmassRes.py
@@ -12,8 +12,10 @@ inASCII = "c:/MSV/WorkDirectory/AsciiLandscape.txt"
 outRaster = "C:/Users/lada/Desktop/vejlerne"
 rasterType = "INTEGER"
 
-# Execute ASCIIToRaster
+# Run ASCIIToRaster
 arcpy.ASCIIToRaster_conversion(inASCII, outRaster, rasterType)
+# Set spatial reference:
+arcpy.SpatialReference("WGS_1984_UTM_Zone_32N")  # Not tested yet
 
 # NA the polygons without geese on them
 inRemapTable = "c:/Users/lada/Desktop/reclass.txt"
