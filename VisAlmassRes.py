d6748ba2cd29c7674c5076181568a2ad7009db15
LDalby <lars@bios.au.dk>
2016-04-08 14:05:36 +0200

    Indent

--- VisAlmassRes.py
+++ VisAlmassRes.py
@@ -6,36 +6,50 @@ from arcpy import env
 import arcpy, traceback, sys, time, gc, os
 from arcpy.sa import *
 arcpy.CheckOutExtension("Spatial")
-
+arcpy.env.parallelProcessingFactor = "75%"
 # Set local variables
 inASCII = "c:/MSV/WorkDirectory/AsciiLandscape.txt"
 outRaster = "C:/Users/lada/Desktop/vejlerne"
 rasterType = "INTEGER"
+try:
+	# Run ASCIIToRaster
+	arcpy.ASCIIToRaster_conversion(inASCII, outRaster, rasterType)
+	# Set spatial reference:
+	prj = arcpy.SpatialReference("WGS 1984 UTM Zone 32N")
+	arcpy.DefineProjection_management(outRaster, prj)
 
-# Run ASCIIToRaster
-arcpy.ASCIIToRaster_conversion(inASCII, outRaster, rasterType)
-# Set spatial reference:
-prj = arcpy.SpatialReference("WGS 1984 UTM Zone 32N")
-arcpy.DefineProjection_management(outRaster, prj)
+	# NA the polygons without geese on them
+	inRemapTable = "c:/Users/lada/Desktop/reclass.txt"
+	# Execute Reclassify
+	reclsRaster = ReclassByTable(outRaster, inRemapTable,"Polyref","Polyref","AvgNumber","NODATA")
+	reclsRaster.save("c:/Users/lada/Desktop/vejlerneGeese")
+	# Import the roosts
+	# Set the local variables
+	in_Table = "c:/Users/lada/Desktop/roosts.txt"
+	x_coords = "ALong"
+	y_coords = "ALat"
+	out_Layer = "roost_layer"
+	saved_Layer = "C:/Users/lada/Desktop/roosts.lyr"
 
-# NA the polygons without geese on them
-inRemapTable = "c:/Users/lada/Desktop/reclass.txt"
-# Execute Reclassify
-reclsRaster = ReclassByTable(outRaster, inRemapTable,"Polyref","Polyref","AvgNumber","NODATA")
-reclsRaster.save("c:/Users/lada/Desktop/vejlerneGeese")
+	# Make the XY event layer...
+	arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, prj)
 
-# ---------------------------------------------------------------- #
-# Add the roosts as points on the map:
-# Set local variables
-out_path = "C:/output"
-out_name = "habitatareas.shp"
-geometry_type = "POINT"
-# template = "study_quads.shp"
-has_m = "DISABLED"
-has_z = "DISABLED"
+	# Print the total rows
+	print arcpy.GetCount_management(out_Layer)
 
-# Use Describe to get a SpatialReference object
-spatial_reference = arcpy.Describe(outRaster).spatialReference
+	# Save to a layer file
+	arcpy.SaveToLayerFile_management(out_Layer, saved_Layer)
+except:
+	tb = sys.exc_info()[2]
+	tbinfo = traceback.format_tb(tb)[0]
+	pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n     " +        str(sys.exc_type) + ": " + str(sys.exc_value) + "\n"
+	msgs = "ARCPY ERRORS:\n" + arcpy.GetMessages(2) + "\n"
 
-# Execute CreateFeatureclass
-arcpy.CreateFeatureclass_management(out_path, out_name, geometry_type, has_m, has_z, spatial_reference)
+	arcpy.AddError(msgs)
+	arcpy.AddError(pymsg)
+
+	print msgs
+	print pymsg
+
+	arcpy.AddMessage(arcpy.GetMessages(1))
+	print arcpy.GetMessages(1)
